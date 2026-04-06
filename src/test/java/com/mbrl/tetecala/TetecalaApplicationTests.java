2d97a33446d0ed9d84d5c46f7eca260f31f3e98d
CesarTokai <cgonzalez@tokai.com.mx>
2026-04-06 17:09:15 -0600

    Firts Commit

---
+++ src/test/java/com/mbrl/tetecala/TetecalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbrl.tetecala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetecalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
